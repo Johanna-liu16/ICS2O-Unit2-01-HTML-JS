318718cb9457190967485206b1984b08364041bd
Johanna-liu16 <99354992+Johanna-liu16@users.noreply.github.com>
2022-03-08 18:47:21 +0000

    added code

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@
 // Created by: Johanna Liu
 // Created on: Mar 2022
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  // this function displays "Hello, World!"
+
+  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>";
+}
